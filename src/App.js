daaedaf458125b1757959de400765f0507cc4e38
lhernandez19 <diegotapia1987@gmail.com>
2022-03-13 17:04:47 -0500

    completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import Hello from "./components/hello";
+import Card from "./components/card";
+import Footer from "./components/Footer"
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<React.Fragment>
+			<Container maxWidth="sm">
+				<Hello />
+				<Card />
+        <Footer/>
+			</Container>
+		</React.Fragment>
+	);
 }
 
 export default App;
---
+++ src/components/hello.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+class Hello extends Component {
+	state = {};
+	render() {
+		return (
+			<React.Fragment>
+				<h1>React Web App</h1>
+				<h4>TECHNICAL REPORTING</h4>
+				<p>
+					A technical report is a document that describes the process, progress,
+					or results of technical or scientific research or the state of a
+					technical or scientific research problem. It might also include
+					recommendations and conclusions of the research.
+				</p>
+			</React.Fragment>
+		);
+	}
+}
+
+export default Hello;
